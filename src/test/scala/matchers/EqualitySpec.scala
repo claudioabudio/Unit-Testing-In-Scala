79e3eda86e3a8e45480798aea48918d2dc71e13c
Claudio Abud <ca9234@us.att.com>
2021-10-02 14:25:31 -0500

    added examples of using Equality matchers

---
+++ src/test/scala/matchers/EqualitySpec.scala
@@ -0,0 +1,44 @@
+package matchers
+
+import com.h2.services.Currency
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class EqualitySpec extends AnyFlatSpec with Matchers {
+  behavior of "Currency Equals"
+  it should "match two 10 Usd currencies as equal when using the should equal matchers" in {
+    val currency1 : Currency = "10 USD"
+    val currency2 : Currency = "10 USD"
+
+    currency1 should equal (currency2)
+  }
+
+  it should "match two 10 Usd currencies as equal when using the should be matchers" in {
+    val currency1 : Currency = "10 USD"
+    val currency2 : Currency = "10 USD"
+
+    currency1 should be (currency2)
+  }
+
+  it should "match two 10 Usd currencies as equal when using the should === matchers" in {
+    val currency1 : Currency = "10 USD"
+    val currency2 : Currency = "10 USD"
+
+    currency1 should === (currency2)
+  }
+
+  it should "match two 10 Usd currencies as equal when using the shouldEqual matchers" in {
+    val currency1 : Currency = "10 USD"
+    val currency2 : Currency = "10 USD"
+
+    currency1 shouldEqual currency2
+  }
+
+  it should "match two 10 Usd currencies as equal when using the shouldBe matchers" in {
+    val currency1 : Currency = "10 USD"
+    val currency2 : Currency = "10 USD"
+
+    currency1 shouldBe currency2
+  }
+
+}
