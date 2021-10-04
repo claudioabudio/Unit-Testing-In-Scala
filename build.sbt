2faf264f4416717d10108ea68a03a214920c9398
claudio Abud <clau@gmail.com>
2021-10-04 09:22:02 -0500

    bumped up sbt version in build.properties, older version prints strange characters when running in a windows terminal, implemented WithFixtureParamSpec

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "Unit Testing in Scala"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.13.6"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % "test"
--- src/test/scala/fixtures/WithFixtureParamSpec.scala
+++ src/test/scala/fixtures/WithFixtureParamSpec.scala
@@ -1,11 +1,46 @@
 package fixtures
 
+import com.h2.services.Currency
+import fixtures.RandomCurrencies.{currencies, currencyCodes, random}
 import org.scalatest.Outcome
 import org.scalatest.flatspec.FixtureAnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class WithFixtureParamSpec extends FixtureAnyFlatSpec with Matchers {
-  override protected def withFixture(test: OneArgTest): Outcome = ???
+import scala.util.Random
 
-  override type FixtureParam = this.type
+class WithFixtureParamSpec extends FixtureAnyFlatSpec with Matchers {
+  private val random = new Random
+  private var currencies: List[Currency] = List.empty
+  private val currencyCodes = List("CAD", "USD", "EUR", "SGD")
+
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    currencies = (1 to random.between(100, 201)).map(_ => {
+      val randomAmount = random.between(10, 101)
+      val randomCurrencyCode = currencyCodes(random.between(0, currencyCodes.length))
+      val currency: Currency = s"$randomAmount $randomCurrencyCode"
+      currency
+    }).toList
+    try {
+        withFixture(test.toNoArgTest(FixtureParam(currencies)))
+
+    } finally {
+      info("removing currencies")
+      currencies = List.empty
+    }
+
+  }
+
+  case class FixtureParam(currencies: List[Currency])
+
+  behavior of "Testing Random Currencies with withFixture"
+  it should "have a total dollar cost > 10 dollars" in {
+    fixtureParam =>
+      fixtureParam.currencies.map(_.costInDollars.amount).sum should be > 10
+  }
+
+  it should "not have any currency with code INR or NZD" in {
+    fixtureParam =>
+      fixtureParam.currencies should contain noneOf ("INR", "NZD")
+  }
+
 }
